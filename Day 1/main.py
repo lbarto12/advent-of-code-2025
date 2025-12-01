8117cc04b669adad2a9548a881774162c2a32d05
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2025-12-01 16:38:49 -0500

    Initial Commit

---
+++ Day 1/main.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+
+Direction = str
+Offset = int
+
+with open("input.txt") as file:
+    instructions: List[Tuple[Direction, Offset]] = [(a[0], int(a[1:])) for a in [i.strip() for i in file.readlines()]]
+
+    # Part 1
+    p1: int = 50
+    print("Part 1:", sum((p1 := (p1 + (offset if direction == "R" else -offset)) % 100) == 0 for direction, offset in instructions))
+
+    # Part 2
+    p2: int = 50
+    print("Part 2:", sum((p2 := (p2 + (1 if direction == "R" else -1)) % 100) == 0 for direction, offset in instructions for _ in range(offset)))
