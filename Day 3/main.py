2cbe607add04f1577cb1ee2e2e2dbefd96912124
lbarto12 <74741908+lbarto12@users.noreply.github.com>
2025-12-03 00:37:53 -0500

    Day 3

---
+++ Day 3/main.py
@@ -0,0 +1,30 @@
+from typing import List, Tuple
+
+def max_and_location(s: str) -> Tuple[str, int]:
+    mx, mi = '-1', -1
+    for i, c in enumerate(s):
+        if int(c) > int(mx):
+            mx, mi = c, i
+    return mx, mi
+
+
+with open("input.txt") as file:
+    lines: List[str] = [i.strip() for i in file.readlines()]
+    
+    j1 = j2 = 0
+    for line in lines:
+        # Part 1
+        first, f_idx = max_and_location(line[:-1])
+        second, _ = max_and_location(line[f_idx + 1:])
+        j1 += int(first + second)
+
+        # Part 2
+        constructed, boundary = '', 0
+        for i in range(-11, 1, 1):
+            nxt, idx = max_and_location(line[boundary:i] if i else line[boundary:])
+            boundary += idx + 1
+            constructed += nxt
+        j2 += int(constructed)
+
+    print("Part 1:", j1)
+    print("Part 2:", j2)
